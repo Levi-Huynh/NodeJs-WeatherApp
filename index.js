225512d6250ce6ac8d1d17343b0d031a60143a7f
Levi-Huynh <Levi_huynh@yahoo.com>
2019-05-17 16:05:58 -0700

    debug heroku

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 //require dotenv first just to make sure everything has access to it
-const envReader = require('dotenv');
-envReader.config(); //reads .env and merges it into process.env
+// const envReader = require('dotenv');
+// envReader.config(); 
+//reads .env and merges it into process.env
 
 // require('dotenv').config();
-const server = require('./server.js');
+const app= require('./server.js');
 
 //below, allows PORT (variable assigned by Heroku or provide 4000). Then add commit and push changes so heroku sees it.
 //WHO IS ADDING PORT VALUE?: HEROKU
 
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
+app.listen(3000, () => {
   console.log('\n*** Server Running on http://localhost:3000 ***\n');
 });
